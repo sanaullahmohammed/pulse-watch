a07aa6d23c1f22361cd33be286a3656f29222d3a
sanaullahmohammed <sanaullah.mohammad100@gmail.com>
2024-12-25 22:27:11 +0530

    feat: fix the deployment for gh-pages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/{repository-name}/",
+  base: "/pulse-watch-plivo-hiring-assignment/",
   plugins: [react()],
   resolve: {
     alias: {
