2b7b44066501eb32be3d634a98c02e9892f9e673
sanaullahmohammed <sanaullah.mohammad100@gmail.com>
2024-12-25 22:09:46 +0530

    feat: add gh-pages deploy for fe

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import {
   Routes,
   Route,
   Navigate,
+  HashRouter,
 } from "react-router-dom";
 import AddTeamMembers from "./components/TeamMembers/AddTeamMembers";
 import CreateService from "./components/Services/CreateService";
@@ -41,124 +42,126 @@ const App = () => {
   };
 
   return (
-    <Router>
-      <Routes>
-        {/* Auth page */}
-        <Route path="/signup" element={<SignUpFlow />} />
+    <HashRouter>
+      <Router>
+        <Routes>
+          {/* Auth page */}
+          <Route path="/signup" element={<SignUpFlow />} />
 
-        {/* Public status page */}
-        <Route path="/" element={<PublicStatus />} />
+          {/* Public status page */}
+          <Route path="/" element={<PublicStatus />} />
 
-        {/* Root route redirects to dashboard */}
-        <Route
-          path="/dashboard"
-          element={
-            <Dashboard
-              teams={teams}
-              teamMembers={teamMembers}
-              services={services}
-              incidents={incidents}
-              loading={isLoading}
-              error={error}
-            />
-          }
-        />
-
-        {/* Team-related routes */}
-        <Route path="/teams">
+          {/* Root route redirects to dashboard */}
           <Route
-            path="add-members"
-            element={<AddTeamMembers teams={teams} />}
-          />
-          <Route
-            path="create"
-            element={<CreateTeam organizationId={organizationId} />}
-          />
-          <Route
-            path="view-members"
+            path="/dashboard"
             element={
-              <ViewTeamMembers
-                teamMembers={teamMembers}
+              <Dashboard
                 teams={teams}
-                error={error}
-                loading={isLoading}
-                organizationId={organizationId}
-              />
-            }
-          />
-          <Route
-            path="edit-member/:teamId/:userId"
-            element={<UpdateTeamMembers teams={teams} />}
-          />
-        </Route>
-
-        {/* Service-related routes */}
-        <Route path="/services">
-          <Route
-            path="create"
-            element={<CreateService organizationId={organizationId} />}
-          />
-          <Route
-            path="manage"
-            element={
-              <ServiceManagement
+                teamMembers={teamMembers}
                 services={services}
-                loading={isLoading}
-                error={error}
-                orgId={organizationId}
-                onServiceDeleted={handleServiceDeleted}
-              />
-            }
-          />
-          <Route path="update/:serviceId" element={<UpdateService />} />
-        </Route>
-
-        {/* Incident-related routes */}
-        <Route path="/incidents">
-          <Route
-            path="manage"
-            element={
-              <IncidentManagement
                 incidents={incidents}
-                services={services}
                 loading={isLoading}
                 error={error}
               />
             }
           />
-          <Route
-            path="manage/:serviceId/:incidentId"
-            element={
-              <UpdateIncident
-                services={services}
-                organizationId={organizationId}
-              />
-            }
-          />
-          <Route
-            path="manage/:serviceId/new"
-            element={
-              <ReportIncident
-                organizationId={organizationId}
-                services={services}
-              />
-            }
-          />
-          <Route
-            path="report"
-            element={
-              <ReportIncident
-                organizationId={organizationId}
-                services={services}
-              />
-            }
-          />
-        </Route>
 
-        {/* Catch all unknown routes */}
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </Router>
+          {/* Team-related routes */}
+          <Route path="/teams">
+            <Route
+              path="add-members"
+              element={<AddTeamMembers teams={teams} />}
+            />
+            <Route
+              path="create"
+              element={<CreateTeam organizationId={organizationId} />}
+            />
+            <Route
+              path="view-members"
+              element={
+                <ViewTeamMembers
+                  teamMembers={teamMembers}
+                  teams={teams}
+                  error={error}
+                  loading={isLoading}
+                  organizationId={organizationId}
+                />
+              }
+            />
+            <Route
+              path="edit-member/:teamId/:userId"
+              element={<UpdateTeamMembers teams={teams} />}
+            />
+          </Route>
+
+          {/* Service-related routes */}
+          <Route path="/services">
+            <Route
+              path="create"
+              element={<CreateService organizationId={organizationId} />}
+            />
+            <Route
+              path="manage"
+              element={
+                <ServiceManagement
+                  services={services}
+                  loading={isLoading}
+                  error={error}
+                  orgId={organizationId}
+                  onServiceDeleted={handleServiceDeleted}
+                />
+              }
+            />
+            <Route path="update/:serviceId" element={<UpdateService />} />
+          </Route>
+
+          {/* Incident-related routes */}
+          <Route path="/incidents">
+            <Route
+              path="manage"
+              element={
+                <IncidentManagement
+                  incidents={incidents}
+                  services={services}
+                  loading={isLoading}
+                  error={error}
+                />
+              }
+            />
+            <Route
+              path="manage/:serviceId/:incidentId"
+              element={
+                <UpdateIncident
+                  services={services}
+                  organizationId={organizationId}
+                />
+              }
+            />
+            <Route
+              path="manage/:serviceId/new"
+              element={
+                <ReportIncident
+                  organizationId={organizationId}
+                  services={services}
+                />
+              }
+            />
+            <Route
+              path="report"
+              element={
+                <ReportIncident
+                  organizationId={organizationId}
+                  services={services}
+                />
+              }
+            />
+          </Route>
+
+          {/* Catch all unknown routes */}
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </Router>
+    </HashRouter>
   );
 };
 
